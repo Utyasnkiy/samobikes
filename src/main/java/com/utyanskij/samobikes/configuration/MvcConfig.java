3a7a97f0933ac1a04f124b46f281bd23f65a156d
home-pc <25072002d@mail.ru>
2024-05-06 22:49:42 +0300

    add comments

--- src/main/java/com/utyanskij/samobikes/configuration/MvcConfig.java
+++ src/main/java/com/utyanskij/samobikes/configuration/MvcConfig.java
@@ -11,20 +11,25 @@ import java.nio.file.Paths;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Определяем директорию для фотографий велосипедов
         String dirBikeName = "photos/bike-photos";
         Path bikePhotosDir = Paths.get(dirBikeName);
 
+        // Определяем директорию для фотографий запчастей
         String dirPartName = "photos/part-photos";
         Path partPhotosDir = Paths.get(dirPartName);
 
+        // Получаем абсолютный путь к директориям фотографий
         String bikePhotosPath = bikePhotosDir.toFile().getAbsolutePath();
         String partPhotosPath = partPhotosDir.toFile().getAbsolutePath();
 
+        // Добавляем обработчики ресурсов для фотографий велосипедов
         registry.addResourceHandler("/" + dirBikeName + "/**")
                 .addResourceLocations(
                         "file:/" + bikePhotosPath + "/",
                         "file:/home/samobikes/samobikes_app/samobikes/" + dirBikeName + "/"
                 );
+        // Добавляем обработчики ресурсов для фотографий запчастей
         registry.addResourceHandler("/" + dirPartName + "/**")
                 .addResourceLocations(
                         "file:/" + partPhotosPath + "/",
